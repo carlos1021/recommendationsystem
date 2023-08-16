3dabfc98b89e4bd736046b9fea59e5f48b188412
carlos1021 <carlos01oscar@berkeley.edu>
2023-08-15 17:46:39 -0700

    8/15/2023 exercise: us

--- desktop.py
+++ desktop.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+import time
 
 #Creating the window object
 window = tk.Tk()
@@ -12,6 +15,38 @@ new_Label = tk.Label(window, text = "Vist the Recommendation System Desktop App 
 
 #Place the label onto the window
 new_Label.grid(column= 0, row = 0)
+
+#Create a progress bar widget
+progress_bar = ttk.Progressbar(window, orient = "horizontal", length=300, maximum = 100, mode="determinate")
+#Place the progress bar widget onto the window
+progress_bar.grid(column = 0, row = 5)
+
+def update_progress_label():
+    return f"Current Progress: {progress_bar['value']}%"
+
+value_label = ttk.Label(window, text=update_progress_label())
+value_label.grid(column=0, row=1, columnspan=2)
+
+
+def progress():
+    if progress_bar['value'] < 100:
+        progress_bar['value'] += 20
+        value_label['text'] = update_progress_label()
+    else:
+        showinfo(message='The progress completed!')
+
+
+
+
+#Create start button
+start_button = ttk.Button(window, text = "START", command = progress)
+#Place the start button onto the window
+start_button.grid(column = 0, row = 8, padx = 10, pady = 10)
+#Create start button
+stop_button = ttk.Button(window, text = "STOP", command = progress_bar.stop)
+#Place the start button onto the window
+stop_button.grid(column = 2, row = 8, padx = 10, pady = 10)
+
 
 window.mainloop()
 
